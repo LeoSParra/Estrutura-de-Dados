96f73e32c2dac08390be6aa18e82ad6d4dcc40fa
LeoSParra <leonardoparra1k@gmail.com>
2023-02-28 07:48:41 -0300

    commitando

--- Revisão/exe2.js
+++ Revisão/exe2.js
@@ -1,15 +1,22 @@
 function exe2(){
     let vetor = []
-
+    let num = 100
     while (num != 0){
-        let num = Number(prompt(`1 - inserir \n 2 - remover \n 3 - somar ímpar ou par \n 4 - somar de um ponto ao outro \n 0 - sair do programa`))
-        if (num = 1){
+        num = Number(prompt(`1 - inserir \n 2 - remover \n 3 - somar ímpar ou par \n 4 - somar de um ponto ao outro \n 0 - sair do programa`))
+        if (num == 1) {
             inserir(vetor)
         } 
-        else if (num == 2){
+        else if (num == 2) {
             remover(vetor)
         }
+        else if (num == 3) {
+            somarImparPar(vetor)
+        }
+        else if (num == 4) {    
+            somarIndice(vetor)
+        }
     }
+    console.log(`${vetor}`)
 }
 
 function inserir(vetor){
@@ -21,10 +28,42 @@ function inserir(vetor){
 }
 
 function remover(vetor){
-    let quantidade = (Number(prompt(`Informe qual o índice a ser removido: `)))
-    vetor.splice(quantidade, quantidade)
+    let indice = (Number(prompt(`Informe qual o índice a ser removido: `)))
+    if (indice == 0) {
+        vetor.shift()
+    }
+    else {
+        vetor.splice(indice, indice - 1)
+    }
 }
 
-function somar(vetor){
-    let 
+function somarImparPar(vetor){
+    let escolha = Number(prompt(`1 - impar, 2 - par: `))
+    let soma = 0
+    if (escolha == 2){
+        for (let i = 0; i < vetor.length; i++){
+            if (vetor[i] % 2 == 0){
+                soma += vetor[i]
+            }
+        }
+        console.log(`${soma}`)
+    }
+    if (escolha == 1){
+        for (let i = 0; i < vetor.length; i++){
+            if (vetor[i] % 2 != 0){
+                soma += vetor[i]
+            }
+        }
+        console.log(`${soma}`)
+    }
+}
+
+function somarIndice(vetor){
+    let inicial = Number(prompt(`Insira o indice inicial: `))
+    let final = Number(prompt(`Insira o indice final: `))
+    let soma = 0
+    for (let i = inicial; i < vetor[final]; i++){
+        soma += vetor[i]
+    }
+    console.log(`${soma}`)
 }
